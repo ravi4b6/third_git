128ae54e35541eee07bd8f25cca57742dfef871f
Ravi Teja <raviteja.valaboju@gmail.com>
2020-12-14 16:08:31 +0530

    American server

---
+++ GIT_Example/src/Three.java
@@ -0,0 +1,13 @@
+
+public class Three {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("One");
+		System.out.println("Two");
+		System.out.println("Three");
+		
+	}
+
+}
---
+++ GIT_Example/src/Two.java
@@ -0,0 +1,13 @@
+
+public class Two {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("One");
+		System.out.println("Two");
+		System.out.println("Three");
+		
+	}
+
+}
